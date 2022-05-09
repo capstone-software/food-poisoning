666a64fa52d6a50e3d8969bedc2aa35abbab31f6
chungchung <61677941+piaochung@users.noreply.github.com>
2022-05-09 18:24:00 +0900

    Feat: "추가 MyRestaurant 서비스 코드 작성 (#8)"

---
+++ backend/src/main/java/core/backend/myRestaurant/exception/MyRestaurantNotFoundException.java
@@ -0,0 +1,6 @@
+package core.backend.myRestaurant.exception;
+
+import core.backend.global.error.exception.CustomException;
+
+public class MyRestaurantNotFoundException extends CustomException {
+}
