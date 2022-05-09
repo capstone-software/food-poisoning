d272fd39641f13971aa28e18dd854c69ceaa4da1
chungchung <61677941+piaochung@users.noreply.github.com>
2022-05-09 14:13:31 +0900

    Feat: "추가 tag 서비스 코드 (#11)"

---
+++ backend/src/main/java/core/backend/tag/repository/TagRepository.java
@@ -0,0 +1,9 @@
+package core.backend.tag.repository;
+
+import core.backend.tag.domain.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
